e72210a9ae9e7417ac2934a462277ad6753934a4
rajashekerreddy <rajasheker.g@wavespot.net>
2014-02-12 16:12:24 +0530

    changed

--- spec/models/attendence_spec.rb
+++ spec/models/attendence_spec.rb
@@ -2,7 +2,7 @@ require_relative '../spec_helper'
 describe Attendence do
    describe "#score" do
 	  	it "returns 0 for all gutter game" do
-	    	attendence = Attendence.last
+	    	attendence = Attendence.last 
 	    	puts attendence.year.should == "1"
 	  	end
 	end  
